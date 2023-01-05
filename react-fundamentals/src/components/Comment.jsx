25c068c5b24f1434c0c7677f7d35c86b95543aa0
marcus_feitosa <marcusfeitosa02@gmail.com>
2023-01-05 17:50:07 -0300

    Validação do formulário

--- react-fundamentals/src/components/Comment.jsx
+++ react-fundamentals/src/components/Comment.jsx
@@ -3,6 +3,11 @@ import {ThumbsUp, Trash} from 'phosphor-react'
 import { Avatar } from './Avatar'
 
 export function Comment(props){
+    function handleDeleteComment(){
+    props.onDeleteComment(props.content);
+    }
+
+
     return(
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://github.com/marcus-feitosa.png"/>
@@ -15,7 +20,7 @@ export function Comment(props){
                             <time title="11 de Maio às 8:13 " dateTime="2022-05-11 08:13:30">Cerca de 1h atrás</time>
                         </div>
                         
-                        <button title="Deletar comentário">
+                        <button onClick={handleDeleteComment} title="Deletar comentário">
                             <Trash size={24}/>
                         </button>
                     </header>
--- react-fundamentals/src/components/Post.jsx
+++ react-fundamentals/src/components/Post.jsx
@@ -23,6 +23,7 @@ export function Post(props){
         addSuffix: true
        
     })
+    const isNewCommentEmpty = newCommentText.length == 0;
 
     function handleCreateNewComment(){
         event.preventDefault()
@@ -34,8 +35,19 @@ export function Post(props){
     }
 
     function handleNewCommentChange(){
-         setNewCommentText(event.target.value)
-    
+        event.target.setCustomValidity('');
+         setNewCommentText(event.target.value);
+    }
+
+    function handleNewCommentInvalid(){
+        event.target.setCustomValidity('Esse campo é obrigatório!');
+    }
+
+    function deleteComment(commentToDelete){
+        const commentsWithoutDeletedOne = comments.filter(comment => {
+            return comment !== commentToDelete;
+        })
+        setComments(commentsWithoutDeletedOne);
     }
     
     return(
@@ -69,16 +81,23 @@ export function Post(props){
             <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <strong>Deixe seu feedback</strong>
 
-                <textarea name="comment" placeholder="Deixe um comentário" value={newCommentText} onChange={handleNewCommentChange}/>
+                <textarea 
+                name="comment" 
+                placeholder="Deixe um comentário" 
+                value={newCommentText} 
+                onChange={handleNewCommentChange}
+                onInvalid={handleNewCommentInvalid}
+                required
+                />
                 <footer>
-                <button type="submit">Publicar</button>
+                <button type="submit" disabled={isNewCommentEmpty}>Publicar</button>
                 </footer>
                
             </form>
 
             <div className={styles.commentList}>
                {comments.map(comment => {
-                return <Comment key={comment} content={comment} />
+                return <Comment key={comment} content={comment} onDeleteComment={deleteComment} />
                })}
             </div>
         </article>
